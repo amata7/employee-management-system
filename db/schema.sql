1de09bc8e11f9af6b65ca7b973d68b170a36fda2
amata7 <adrianmata@live.com>
2021-07-04 02:53:00 -0700

    deleted unncessary file, added foreign keys to eys to the schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,12 @@ CREATE TABLE role (
   id INT PRIMARY KEY auto_increment,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
+  CONSTRAINT fk_department
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+	ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -20,7 +25,17 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
+  manager_id INT,
+  CONSTRAINT fk_role
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+	ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT fk_manager
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(manager_id)
+	ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
 SELECT * FROM department;
--- seeds/hw.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS traveller_db;
-CREATE DATABASE traveller_db;
-
-SELECT * FROM location;
-SELECT * FROM traveler;
-SELECT * FROM trip;
